b171f394f97a6586696f772bca44f195e140403f
Garrett Anderson <gman.anderson2001@gmail.com>
2023-01-16 15:44:44 -0700

    Fixed server issues

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const { ApolloServer } = require("apollo-server-express");
-
+const { authMiddleware } = require("./utils/auth");
 const { typeDefs, resolvers } = require("./schemas");
 const db = require("./config/connection");
 // const routes = require('./routes');
@@ -11,6 +11,7 @@ const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: authMiddleware,
 });
 
 app.use(express.urlencoded({ extended: true }));
@@ -27,9 +28,9 @@ if (process.env.NODE_ENV === "production") {
 //   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 // });
 
-db.once("open", () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+// db.once("open", () => {
+//   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+// });
 
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
