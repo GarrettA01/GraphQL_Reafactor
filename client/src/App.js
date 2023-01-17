2bc8c9e441ff63ae8f9587b957bd7ce1ed9a1dbb
Garrett Anderson <gman.anderson2001@gmail.com>
2023-01-17 09:56:27 -0700

    Fixed App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,36 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 import SearchBooks from "./pages/SearchBooks";
 import SavedBooks from "./pages/SavedBooks";
 import Navbar from "./components/Navbar";
 
-//Create an Apollo Provider to make every request work with the Apollo server.
-const client = new ApolloClient({
+// this sends graphql operations to our remote endpoint
+const httpLink = createHttpLink({
   uri: "/graphql",
+});
+
+// create link with context to use jwt and pass to apollo
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      // this might solve the token problem
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+// using the apollo client with the created http link
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
